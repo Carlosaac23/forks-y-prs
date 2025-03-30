ba955d1983106d2d5ffe715e77a1e48b78624406
carlosaac23 <carlosaac23@hotmail.com>
2025-03-30 17:33:15 -0500

    resuelto el index.ts para que pase los tests

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,36 @@
-import products from "./products.json";
+import products from './products.json';
 
 class Product {
+  id: number;
+  name: string;
+  price: number;
+
   constructor(name: string, price: number) {
     this.name = name;
     this.price = price;
   }
-  id: number;
-  name: string;
-  price: number;
+
+  static findProductsBelow(precioBase: number) {
+    const productos = products.filter(producto => producto.price < precioBase);
+    return productos;
+  }
 }
 
 class User {
+  name: string;
+  products: Product[] = [];
+
   constructor(name: string) {
     this.name = name;
   }
-  name: string;
-  products: Product[] = [];
+
   addProduct(newProduct: Product) {
     this.products.push(newProduct);
   }
+
   addProducts(newProducts: Product[]) {
     // esto no funciona:
-    this.products.push(newProducts);
+    this.products = this.products.concat(newProducts);
     // pista: push no suma muchos items (agrega de a uno)
   }
 }
